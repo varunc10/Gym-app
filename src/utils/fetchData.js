1987e1772da8bf84714a34b0d57df13658c6944b
Varun Chanda <varunchanda@Varuns-MacBook-Air.local>
2025-03-18 21:24:16 +0530

    minor fixes

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '6cba9264d2msh7c74f3bdd418c70p1ff6a4jsn8b3db6546f3d',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '6cba9264d2msh7c74f3bdd418c70p1ff6a4jsn8b3db6546f3d',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
 };
